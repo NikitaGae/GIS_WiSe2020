faf0787189529113127d02f1a001391497a0a2c8
Nikita <nikita.gaertner@hs-furtwangen.de>
2021-01-13 13:09:29 +0100

    test

--- Aufgabe3.1/script.js
+++ Aufgabe3.1/script.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.P_3_1Server = void 0;
 const Http = require("http");
-const Url = require("url");
 const Mongo = require("mongodb");
 //https://mongodbnetbrowser.herokuapp.com/?u=Testuser&p=Testuser&a=nikita-gis-ist-geil.gl0tb.mongodb.net&n=test&c=Students
 var P_3_1Server;
@@ -35,14 +34,17 @@ var P_3_1Server;
         console.log("Database connection ", log != undefined);
     }
     //function zum vergleichen der eingegeben daten
-    async function vergleichen(_url) {
-        let pathSplit = _url.split("?");
-        let daten = pathSplit[1].split("&");
-        let ntzName = daten[0].split("=");
-        let ntzFirstname = daten[1].split("=");
-        let ntzRegistration = daten[2].split("=");
-        let logInArray = await log.find().toArray();
-        for (let i = 0; i < logInArray.length; i++) {
+    /* async function vergleichen(_url: string): Promise<boolean> {
+
+        let pathSplit: string[] = _url.split("?");
+        let daten: string[] = pathSplit[1].split("&");
+        let ntzName: string[] = daten[0].split("=");
+        let ntzFirstname: string[] = daten[1].split("=");
+        let ntzRegistration: string[] = daten[2].split("=");
+
+        let logInArray: LogIn[] = await log.find().toArray();
+
+        for (let i: number = 0; i < logInArray.length; i++) {
             if (ntzName[1] == (logInArray[i].name)) {
                 if (ntzFirstname[1] == (logInArray[i].firstname)) {
                     if (ntzRegistration[1] == logInArray[i].registration) {
@@ -53,38 +55,36 @@ var P_3_1Server;
             }
         }
         return false;
-    }
+    } */
     //hier werden die server antworten geschrieben je nachdem auf welcher html seite wir uns befinden
     async function handleRequest(_request, _response) {
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        if (_request.url) {
-            let url = Url.parse(_request.url, true);
-            let path = url.pathname;
+        /* if (_request.url) {
+
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            let path: String | null = url.pathname;
+
             if (path == "/login") {
                 if (await vergleichen(url.path) == false) {
                     _response.write("User nicht gefunden überprüfen sie ihre eingabe");
-                }
-                else {
+                } else {
                     _response.write("User gefunden");
                 }
-            }
-            else if (path == "/registrieren") {
-                if (await vergleichen(url.path) == false) {
+            } else if (path == "/registrieren") {
+                if ( await vergleichen(url.path) == false) {
                     log.insertOne(url.query);
                     _response.write("User erstellt");
-                }
-                else {
+                } else {
                     _response.write("User existiert schon");
                 }
-            }
-            else if (path == "/nutzer") {
-                let logInArray = await log.find().toArray();
-                let logInArrayJSON = JSON.stringify(logInArray);
+            } else if (path == "/nutzer") {
+                let logInArray: LogIn[] = await log.find().toArray();
+                let logInArrayJSON: string = JSON.stringify(logInArray);
                 _response.write(logInArrayJSON);
             }
-        }
+        } */
         _response.write("test");
         _response.end();
     }
