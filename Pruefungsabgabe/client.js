7891f38d732cba7721738829fe063b0a4687c936
Nikita <nikita.gaertner@hs-furtwangen.de>
2021-07-11 17:39:10 +0200

    FINAL

--- Pruefungsabgabe/client.js
+++ Pruefungsabgabe/client.js
@@ -8,8 +8,8 @@ var P_3_1Server;
     async function main() {
         let location = window.location.pathname.split("/");
         let teil = location[location.length - 1];
-        //let url: string = "https://testgiswise2020.herokuapp.com";
-        let url = "http://localhost:8100";
+        let url = "https://testgiswise2020.herokuapp.com";
+        //let url: string = "http://localhost:8100";
         switch (teil) {
             case "registrieren.html":
                 url += "/registrieren";
